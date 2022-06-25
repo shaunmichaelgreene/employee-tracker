a3d6ae2256124cc2e0a98ea055fdb6a25d6136a3
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-06-25 11:59:23 -0400

    database created and seeded

--- db/schema.sql
+++ db/schema.sql
@@ -3,22 +3,24 @@ DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS department;
 
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO INCREMENT NOT NULL,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL
 );
+
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTO INCREMENT NOT NULL,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
     CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE CASCADE
 );
+
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY AUTO INCREMENT NOT NULL,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
     manager_id INTEGER,
     constraint fk_role FOREIGN KEY(role_id) REFERENCES role(id) ,
-    constraint fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NUL:
+    constraint fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -12,29 +12,29 @@ INSERT INTO department
 INSERT INTO role 
     (id, title, salary, department_id)
     VALUES
-    (1, 'President & CEO', 250,000, 1),
-    (2, 'Operations Manager', 125,000, 2),
-    (3, 'Business Development Manager', 100,000, 3),
-    (4, 'Account Manager', 75,000, 3),
-    (5, 'Chief Legal Counsel', 150,000, 5),
-    (6, 'IT Administrator', 95,000, 7),
-    (7, 'Assistant to the Regional Manager', 60,000, 6),
-    (8, 'Process Engineer', 90,000, 4),
-    (9, 'Facilites Manager', 65,000, 2),
-    (10, 'Office Manager', 70,000, 6);
+    (1, 'President & CEO', 250000, 1),
+    (2, 'Operations Manager', 125000, 2),
+    (3, 'Business Development Manager', 100000, 3),
+    (4, 'Account Manager', 75000, 3),
+    (5, 'Chief Legal Counsel', 150000, 5),
+    (6, 'IT Administrator', 95000, 7),
+    (7, 'Asst to the Regional Manager', 60000, 6),
+    (8, 'Process Engineer', 90000, 4),
+    (9, 'Facilites Manager', 65000, 2),
+    (10, 'Office Manager', 70000, 6);
 
 INSERT INTO employee 
     (id, first_name, last_name, role_id, manager_id)
     VALUES
-    (1, 'Shaun', 'Greene'),
-    (2, 'Alana', 'McKeel', 1),
-    (3, 'Jack', 'Donaghy', 2),
-    (4, 'Jim', 'Halpert', 3),
-    (5, 'Bob', 'Loblaw', 1),
-    (6, 'Maurice', 'Moss', 1),
-    (6, 'Erlich', 'Bachman', 2),
-    (7, 'Dwight', 'Schrute', 2),
-    (8, 'Lucius', 'Fox', 2),
-    (9, 'Ron', 'Swanson', 2),
-    (10, 'Andy', 'Dwyer', 1),
-    (4, 'Anders', 'Holmvik', 3);
+    (1, 'Shaun', 'Greene', 1, NULL),
+    (2, 'Alana', 'McKeel', 2, 1),
+    (3, 'Jack', 'Donaghy', 3, 2),
+    (4, 'Jim', 'Halpert', 4, 3),
+    (5, 'Bob', 'Loblaw', 5, 1),
+    (6, 'Maurice', 'Moss', 6, 1),
+    (7, 'Erlich', 'Bachman', 6, 2),
+    (8, 'Dwight', 'Schrute', 7, 2),
+    (9, 'Lucius', 'Fox', 8, 2),
+    (10, 'Ron', 'Swanson', 9, 2),
+    (11, 'Andy', 'Dwyer', 10, 1),
+    (12, 'Anders', 'Holmvik', 4, 3);
